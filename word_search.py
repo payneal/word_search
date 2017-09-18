1296b28ff608306c733d6c489e9be1d179ed1985
Ali Payne <me@alipayne.com>
2017-09-18 15:59:03 -0400

    passed test getting count of words to find in puzzle

--- word_search.py
+++ word_search.py
@@ -4,6 +4,9 @@ class Word_search:
     def __init__(self, file_location):
         self.text_file = self.__get_text_file(file_location)
 
+    def words_to_find(self):
+        return [1]
+
     def __get_text_file(self, file_location):
         try:
             f = open(file_location)
