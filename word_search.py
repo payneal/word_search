aca446d3725652bf8c5e2a6febc2a67f251d5b22
Ali Payne <me@alipayne.com>
2017-09-18 08:52:13 -0400

    refactored code to throw error when file not found

--- word_search.py
+++ word_search.py
@@ -2,5 +2,10 @@
 
 class Word_search:
     def __init__(self, file_location):
-        raise ValueError("NO FILE FOUND")
-        
+        self.text_file = self.__get_text_file(file_location)
+
+    def __get_text_file(self, file_location):
+        try:
+            f = open(file_location)
+        except:
+            raise ValueError("NO FILE FOUND")
