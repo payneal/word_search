386697f360a658a7be51c230aa029793391a84c4
Ali Payne <me@alipayne.com>
2017-09-19 06:23:50 -0400

    refactor

--- word_search.py
+++ word_search.py
@@ -11,9 +11,9 @@ class Word_search:
 
     def solve(self): 
         for word in self.search_words:
-            self.start_looking(word)
-        
+            self.start_looking(word)        
         return self.answer_string
+
 
     def start_looking(self, word):
             for word_idx, letter in enumerate(word):
@@ -29,29 +29,23 @@ class Word_search:
             for index_col, col in enumerate(row):
                 if letter == self.puzzle[index_row][index_col]:
                     hold.append((index_row,index_col))
-                    if self.check_for_next_letters(word, index_row, index_col, word_idx, hold)  is True:
-                        return True
-                  
+                    if self.check_for_next_letters(
+                            word, index_row, index_col, word_idx, hold)  is True:
+                        return True       
+
 
     def check_for_next_letters(self, word, index_row, index_col, word_idx, hold):
         for i in range(1, len(word)):
-
             if len(self.puzzle) <=  index_col+i:
                 return self.check_backwards(word, index_row, index_col, word_idx, hold) 
-
-                hold = []
-                break
             if self.puzzle[index_row][index_col+i] == word[word_idx+ i]:
                 hold.append((index_row, index_col+i))
             else:
                 break
-        for x in hold:
-            self.answer_string += str(x).replace(" ", "") + " , " 
-        self.answer_string = self.answer_string[:-3]
-        return True
+        return self.create_answer_from_collected_cords(hold) 
+
 
     def check_backwards(self, word, index_row, index_col, word_idx, hold):
-
         for i in range(1, len(word)):
             if len(self.puzzle) < index_col+i:
                 hold = []
@@ -60,13 +54,15 @@ class Word_search:
                 hold.append((index_row, index_col-i))
             else:
                 break
+        return self.create_answer_from_collected_cords(hold)
+
+
+    def create_answer_from_collected_cords(self, hold):
         for x in hold:
             self.answer_string += str(x).replace(" ", "") + " , " 
         self.answer_string = self.answer_string[:-3]
         return True
  
-
-
 
     def words_to_find(self):
         return self.search_words
