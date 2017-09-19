ae2d3c2ab69f78f7dc2a0c1e08cac5a74cb1c965
Ali Payne <me@alipayne.com>
2017-09-18 21:38:02 -0400

    refactor and pass test of reciving search word thats too small

--- word_search.py
+++ word_search.py
@@ -2,16 +2,16 @@
 
 class Word_search:
     def __init__(self, file_location):
-        self.text_file = self.__get_text_file(file_location)
+        self.__get_text_file(file_location)
+        self.file_location = file_location
         self.search_words = ["search"]
 
-
     def words_to_find(self):
-        return  self.search_words
+        return self.search_words
 
     def __get_text_file(self, file_location):
         self.__verify_crossword_puzzle(file_location)
-
+            
     def __verify_crossword_puzzle(self, file_location):
         try:
             f = open(file_location)
@@ -19,7 +19,19 @@ class Word_search:
             self.__throw_error_message("NO FILE FOUND")
 
         if len(f.read().strip()) == 0:
+            f.close()
             self.__throw_error_message("BLANK FILE")
+        f.close()
+        
+        with open(file_location, mode='r') as f:
+            content = f.readlines()
+        content = [x.strip() for x in content]
+        
+        for x in content:
+            if len(x) < 2:
+                self.__throw_error_message("INVALID SEARCH WORD")
+            
+
 
     def __throw_error_message(self, message):
         raise ValueError(message)
