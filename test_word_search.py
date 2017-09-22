86d595bfeffa701b068ca96f707c91b0f759267f
Ali Payne <me@alipayne.com>
2017-09-22 10:02:13 -0400

    test failed turns out I wasnt done my answers are backwards and it skips answering some words

--- test_word_search.py
+++ test_word_search.py
@@ -16,7 +16,7 @@ class Test_Word_Case(unittest.TestCase):
         with self.assertRaisesRegexp(ValueError, "NO FILE FOUND"): 
             Word_search("./no_file.txt")
 
-    def test_word_search_gets_bad_file_location_throws_error(self):
+    def test_word_search_throws_error_if_file_blank(self):
         with self.assertRaisesRegexp(ValueError, "BLANK FILE"): 
             Word_search("./text_files/empty.txt")
 
@@ -137,6 +137,8 @@ class Test_Word_Case(unittest.TestCase):
         search = Word_search("./text_files/pillar_example_2.txt")
         answer = search.solve()
         expected = "BONES: (0,6) , (0,7) , (0,8) , (0,9) , (0,10) KHAN: (5,9) , (5,8) , (5,7) , (5,6)  KIRK: (4,7) , (3,7) , (2,7) , (1,7) SCOTTY: (0,5) , (1,5) , (2,5) , (3,5) , (4,5) , (5,5) SPOCK: (2,1) , (3,2) , (4,3) , (5,4) , (6,5) SULU: (3,3) , (2,2) , (1,1) , (0,0) UHURA: (4,0) , (3,1) , (2,2) , (1,3) , (0,4)"
+        self.assertEqual(answer, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
