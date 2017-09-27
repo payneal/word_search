420a4899a234f72e69f59c54c8b27e4e3485aac6
Ali Payne <me@alipayne.com>
2017-09-27 18:48:10 -0400

    test passed pillar ex 4

--- test_word_search.py
+++ test_word_search.py
@@ -150,9 +150,12 @@ class Test_Word_Case(unittest.TestCase):
         answer = search.solve()
         expected = "BONES: (0,6) , (0,7) , (0,8) , (0,9) , (0,10) KHAN: (5,9) , (5,8) , (5,7) , (5,6) KIRK: (4,7) , (3,7) , (2,7) , (1,7)"
         self.assertEqual(answer, expected)
-
-
-
+    
+    def test_pillar_example_four(self):
+        search = Word_search("./text_files/pillar_ex/four.txt")
+        answer = search.solve()
+        expected = 'BONES: (0,6) , (0,7) , (0,8) , (0,9) , (0,10) KHAN: (5,9) , (5,8) , (5,7) , (5,6) KIRK: (4,7) , (3,7) , (2,7) , (1,7) SCOTTY: (0,5) , (1,5) , (2,5) , (3,5) , (4,5) , (5,5)'
+        self.assertEqual(answer, expected)
 
 
 if __name__ == '__main__':
