6fed78fb2efb985a3ab4647252688c61e3a58b0e
Ali Payne <me@alipayne.com>
2017-09-27 18:55:33 -0400

    test failed word 6

--- test_word_search.py
+++ test_word_search.py
@@ -163,6 +163,13 @@ class Test_Word_Case(unittest.TestCase):
         expected = "BONES: (0,6) , (0,7) , (0,8) , (0,9) , (0,10) KHAN: (5,9) , (5,8) , (5,7) , (5,6) KIRK: (4,7) , (3,7) , (2,7) , (1,7) SCOTTY: (0,5) , (1,5) , (2,5) , (3,5) , (4,5) , (5,5) SPOCK: (2,1) , (3,2) , (4,3) , (5,4) , (6,5)"
         self.assertEqual(answer, expected)
 
+    def test_pillar_example_six(self):
+        search = Word_search("./text_files/pillar_ex/six.txt")
+        answer = search.solve()
+        expected = "BONES: (0,6) , (0,7) , (0,8) , (0,9) , (0,10) KHAN: (5,9) , (5,8) , (5,7) , (5,6) KIRK: (4,7) , (3,7) , (2,7) , (1,7) SCOTTY: (0,5) , (1,5) , (2,5) , (3,5) , (4,5) , (5,5) SPOCK: (2,1) , (3,2) , (4,3) , (5,4) , (6,5) SULU: (3,3) , (2,2) , (1,1) , (0,0)"
+        self.assertEqual(answer, expected)
+
+
 
 
 
